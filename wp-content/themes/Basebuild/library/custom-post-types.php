ea3e5239e84ced1eab43aec1ab8bbd9ccf41bf04
biip <bm.palma@clix.pt>
2023-06-09 14:26:47 +0100

    remove cpt

--- wp-content/themes/Basebuild/library/custom-post-types.php
+++ wp-content/themes/Basebuild/library/custom-post-types.php
@@ -7,25 +7,3 @@ if( function_exists('acf_add_options_page') ) {
 		'menu_title'	=> 'Global Settings',
 	));	
 } 
-
-// to add custom post types to the sites menu
-add_action( 'init', 'create_post_type' );
-
-function create_post_type() {
-	/** 
-	register_post_type( 'treestory',
-		array(
-			'labels' => array(
-				'name' => __( 'Treestory' ),
-				'singular_name' => __( 'Treestory' )
-			),
-		'public' => true,
-		'show_in_nav_menus' => true,
-		'has_archive' => true,
-		'taxonomies' => array('category', 'post_tag'),
-		'supports' => array('title', 'thumbnail', 'author','editor')
-		// 	'menu_icon' => get_template_directory_uri() . '/assets/images/theme/admin-icons/icon-howtouse.svg'
-		)
-	);
-	*/
-}
