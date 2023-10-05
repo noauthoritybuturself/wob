06e66ca0789314864477a422d87a5a5567711b19
biip <bm.palma@clix.pt>
2023-10-05 13:48:25 +0100

    add social

--- wp-content/themes/Basebuild/footer.php
+++ wp-content/themes/Basebuild/footer.php
@@ -14,9 +14,15 @@
 		<div class="row text-white">
 				<div class="col-md-6">
 
+    
+
 					<ul class="list-unstyled">
 						<li><a href="https://twitter.com/Wo_Bitcoin">Twitter</a></li>
 						<li><a href="https://instagram.com/Wo_Bitcoin">Instagram</a></li>
+						<li><a href="https://t.me/WoBitcoin">Telegram</a></li>
+						<li><a href="https://help.wobitcoin.org/Explore#?zoom=5&lat=40.2476&lng=-3.5713">Help Buttons</a></li>
+						<li><a href="https://www.youtube.com/@WOBitcoin">Youtube</a></li>
+
 					</ul>
 					
 				</div>
@@ -24,6 +30,7 @@
 					<ul class="list-unstyled">
 						<li><a href="https://www.dropbox.com/s/hne3v5kbrarcy0v/POL%C3%8DTICA%20DE%20PRIVACIDAD-wof.pdf?raw=1">Legal</a></li>
 						<li><a href="https://www.dropbox.com/sh/epzwd8b0afvmz7s/AADfB-Zrhpg5Mf0v1Wlp7nRfa?dl=0&preview=cod_etico_wof_2022_00.pdf">Carta principios</a></li>
+						<li><a href="https://www.dropbox.com/scl/fo/satm83lvjqfouww8j3de3/h?rlkey=qxtj791qe83j0z7txi2st22nv&dl=0">Prensa</a></li>
 					</ul>
 				</div>
 			</div>
